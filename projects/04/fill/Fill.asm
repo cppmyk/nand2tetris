1cdf591cca500b5200f0b445f865d1673749d2f1
molockh <molockh@protonmail.com>
2022-09-14 22:18:50 +0300

    Fourth week

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -12,3 +7,73 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Can be refactored but I don't want to do it
+
+(LOOP)
+@SCREEN
+D=A
+@addr
+M=D
+@8192
+D=A
+@n
+M=D
+
+// reset i
+@i 
+M=1
+
+
+@KBD
+D=M
+@DRAW_WHITE
+D;JEQ
+@DRAW_BLACK
+0;JMP
+
+(DRAW_WHITE)
+// condition
+@i 
+D=M
+@n
+D=M-D
+@LOOP
+D;JLT
+
+// set white color
+@addr
+A=M
+M=0
+@addr
+M=M+1
+
+@i
+M=M+1
+
+@DRAW_WHITE
+0;JMP
+
+
+(DRAW_BLACK)
+// condition
+@i 
+D=M
+@n
+D=M-D
+@LOOP
+D;JLT
+
+// set black color
+@addr
+A=M
+M=-1
+@addr
+M=M+1
+
+@i
+M=M+1
+
+@DRAW_BLACK
+0;JMP
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 //
@@ -10,3 +5,44 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@i
+M=1
+
+@R1
+D=M
+@n
+M=D
+
+@R2
+M=0
+
+
+(LOOP)
+// condition
+@i
+D=M
+@n
+D=M-D
+@STOP
+D;JLT
+
+// calc result
+@R0
+D=M
+@R2
+M=M+D
+// increment
+@i
+M=M+1
+@LOOP
+0;JMP
+
+
+(STOP)
+// @R2
+// M=D
+
+(END)
+@END
+0;JMP
